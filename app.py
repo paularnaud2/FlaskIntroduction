c66ac74243fd52b1a07c7f325ae84083f1eb6595
Paul perso <70315271+paularnaud2@users.noreply.github.com>
2020-11-22 15:27:18 +0100

    flake 8 review

--- app.py
+++ app.py
@@ -7,54 +7,46 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = sql(app)
 
-class Todo(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.String(200), nullable=False)
-	date_created = db.Column(db.DateTime, default=datetime.now)
 
-@app.route('/', methods = ['POST', 'GET'])
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-	if request.method == 'POST':
-		task_content = request.form['content']
-		if task_content != '':
-			new_task = Todo(content = task_content)
-			
-			try:
-				db.session.add(new_task)
-				db.session.commit()
-			except:
-				return "Une erreur est survenue lors de l'ajout de la tâche"
-		return redirect('/')
-	else:
-		tasks = Todo.query.order_by(Todo.date_created.desc()).all()
-		# for task in tasks:
-		#	 print(task.date_created)
-		return render_template('index.html', tasks = tasks)
+    if request.method == 'POST':
+        task_content = request.form['content']
+        if task_content != '':
+            new_task = Todo(content=task_content)
+            db.session.add(new_task)
+            db.session.commit()
+        return redirect('/')
+    else:
+        tasks = Todo.query.order_by(Todo.date_created.desc()).all()
+        return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
-	task_to_delete = Todo.query.get_or_404(id)
-	
-	try:
-		db.session.delete(task_to_delete)
-		db.session.commit()
-		return redirect('/')
-	except:
-		return "Une erreur est survenue lors de la suppression de la tâche"
+    task_to_delete = Todo.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
 
-@app.route('/update/<int:id>', methods = ['POST', 'GET'])
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
 def update(id):
-	task = Todo.query.get_or_404(id)
-	if request.method == 'POST':
-		task.content = request.form['content']
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return "Une erreur est survenue lors de la mise à jour de la tâche"
-	else:
-		return render_template('update.html', task = task)
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == '__main__':
-	# app.run(host='192.168.1.106', debug = True)
-	app.run(debug = True)
+    # app.run(host='192.168.1.106', debug = True)
+    app.run(debug=True)
--- app_waitress.py
+++ app_waitress.py
@@ -2,4 +2,4 @@ import waitress
 from app import app
 
 port = '5000'
-waitress.serve(app, url_scheme='https', port = port)
+waitress.serve(app, url_scheme='https', port=port)
